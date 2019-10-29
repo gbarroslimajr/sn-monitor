64b2cc47087a128610d9e01325aead66685f3520
noowy <noowy36@gmail.com>
2019-10-29 11:38:32 +0300

    initial commit

---
+++ src/main/java/com/commerce/sn_monitor/api/DataResolveRestController.java
@@ -0,0 +1,9 @@
+package com.commerce.sn_monitor.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/resolve")
+public class DataResolveRestController
+{
+
+}
---
+++ src/test/java/com/commerce/sn_monitor/SocialNetworkMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commerce.sn_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
