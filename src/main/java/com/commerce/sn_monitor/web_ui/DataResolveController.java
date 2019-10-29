f3819b0d5ce339670ef339d6cf7eea3ec664f08a
AnSoldatkin <anton.soldatkin@odysseusinc.com>
2019-10-29 19:52:55 +0300

    Add: view controller and a couple of templates

--- src/main/java/com/commerce/sn_monitor/web_ui/DataResolveController.java
+++ src/main/java/com/commerce/sn_monitor/web_ui/DataResolveController.java
@@ -1,10 +1,15 @@
 package com.commerce.sn_monitor.web_ui;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/resolve")
 public class DataResolveController
 {
+    public String resolverForm(Model model)
+    {
+        return "resolverForm";
+    }
 }
