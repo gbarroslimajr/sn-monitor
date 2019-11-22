45dc62d20e71e58a7c980e0a965a7af348fcf1bc
noowy <noowy36@gmail.com>
2019-11-22 10:25:52 +0300

    Add: cred encoding and endpoint property

--- src/main/java/com/commerce/sn_monitor/configs/ImlApiConfig.java
+++ src/main/java/com/commerce/sn_monitor/configs/ImlApiConfig.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Base64;
+
 @Configuration
 @ConfigurationProperties(prefix = "com.commerce.sn-monitor.delivery-service")
 @Data
@@ -11,4 +13,11 @@ public class ImlApiConfig
 {
     public String LOGIN;
     public String PASSWORD;
+    public String ENDPOINT;
+
+    public String getBase64EncodedCredentials()
+    {
+        byte[] encodedBytes = Base64.getEncoder().encode((this.LOGIN + this.PASSWORD).getBytes());
+        return new String(encodedBytes);
+    }
 }
--- src/main/java/com/commerce/sn_monitor/services/ImlWebService.java
+++ src/main/java/com/commerce/sn_monitor/services/ImlWebService.java
@@ -1,23 +1,52 @@
 package com.commerce.sn_monitor.services;
 
+import com.commerce.sn_monitor.configs.ImlApiConfig;
 import com.commerce.sn_monitor.domain.iml.ImlOrderDeliveryRequest;
 import com.commerce.sn_monitor.domain.iml.ImlOrderDeliveryResponse;
 import com.commerce.sn_monitor.domain.iml.ImlOrderDeliveryStatus;
 import com.commerce.sn_monitor.domain.iml.ImlOrderDeliveryStatusRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Base64;
 
 @Service
 public class ImlWebService implements OrderDeliveryProcessingService
 {
+
+    private RestTemplate rest;
+    private ImlApiConfig conf;
+
+    public ImlWebService(RestTemplate rest, ImlApiConfig conf)
+    {
+        this.rest = rest;
+        this.conf = conf;
+    }
+
     @Override
     public ImlOrderDeliveryResponse makeDeliveryRequest(ImlOrderDeliveryRequest order)
     {
+
         return null;
     }
 
     @Override
     public ImlOrderDeliveryStatus getDeliveryStatus(ImlOrderDeliveryStatusRequest statusRequest)
     {
+        String statusEndpoint = conf.ENDPOINT + "/GetStatuses";
+
+        HttpHeaders headers = getRequiredHeaders();
+
+        ImlOrderDeliveryStatusRequest requestInfo = new ImlOrderDeliveryStatusRequest();
+
         return null;
+    }
+
+    private HttpHeaders getRequiredHeaders()
+    {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Basic " + conf.getBase64EncodedCredentials());
+        return headers;
     }
 }
