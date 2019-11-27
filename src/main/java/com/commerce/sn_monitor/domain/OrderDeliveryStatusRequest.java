1a795154978a7e787af6eaaa75c1fe08322f3500
noowy <noowy36@gmail.com>
2019-11-27 20:29:49 +0300

    Add: subtypes specification draft for the jackson deserialization

--- src/main/java/com/commerce/sn_monitor/domain/OrderDeliveryStatusRequest.java
+++ src/main/java/com/commerce/sn_monitor/domain/OrderDeliveryStatusRequest.java
@@ -1,5 +1,13 @@
 package com.commerce.sn_monitor.domain;
 
+import com.commerce.sn_monitor.domain.iml.ImlOrderDeliveryStatusRequest;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = ImlOrderDeliveryStatusRequest.class, name = "ImlOrderDeliveryStatusRequest")
+})
 public interface OrderDeliveryStatusRequest
 {
 }
